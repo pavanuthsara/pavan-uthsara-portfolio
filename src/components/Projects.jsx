49fe9ebe8a8783b9aee7e99e4d80ce2847a62373
Pavan Uthsara <uthsarapavan@gmail.com>
2025-07-17 15:59:19 +0530

    hero section v1.0

---
+++ src/components/Projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className="flex justify-center items-center gap-5">
+      <div className="w-1/2">
+        <div>Project 1</div>
+        <div>Project 2</div>
+      </div>
+      <div className="w-1/2 pr-8">
+        <div>Project 3</div>
+        <div>Project 4</div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
