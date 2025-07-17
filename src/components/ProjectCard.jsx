97deea740ea4ccd62c22f365dc75f469de969a31
Pavan Uthsara <uthsarapavan@gmail.com>
2025-07-17 21:41:20 +0530

    project card v1.0

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const ProjectCard = () => {
+const ProjectCard = ({ title, description, image, techstack, source }) => {
   return (
-    <div>ProjectCard</div>
-  )
-}
+    <div className="flex w-80 h-auto justify-start bg-zinc-100 ">
+      <div>
+        <img
+          src={image}
+          href={source}
+          alt={title}
+          className="w-16 h-auto"
+        ></img>
+      </div>
+      <div>
+        <p>{title}</p>
+        <p>{description}</p>
+        <p>{techstack}</p>
+        <p>{source}</p>
+      </div>
+    </div>
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ProjectCard from './ProjectCard'
+import React from "react";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
-    <div className="flex justify-center items-center gap-5 mt-4 pt-16">
-      <div className="w-1/2">
-        <ProjectCard />
-        <div>Project 2</div>
+    <div className="flex flex-row justify-center items-center gap-5 mt-4 pt-16">
+      <div className=" w-1/2 place-items-end">
+        <ProjectCard title={"Freshly.lk"} image={"/img/github-logo.svg"} />
+        <ProjectCard title={"Spare parts selling"} image={"/img/github-logo.svg"} />
       </div>
       <div className="w-1/2 pr-8">
-        <div>Project 3</div>
-        <div>Project 4</div>
+        <ProjectCard title={"Expenses tracker - Android app"} image={"/img/github-logo.svg"} />
+        <ProjectCard title={"Pavan Uthsara - Youtube channel"} image={"/img/github-logo.svg"} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
