1512d160cb29c8d8275d27903b13dbe65eac2d28
Pavan Uthsara <uthsarapavan@gmail.com>
2025-07-18 08:42:26 +0530

    project card v1.2

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { Youtube, Github } from "lucide-react";
 
 const ProjectCard = ({ title, description, image, techstack, source }) => {
   return (
-    <div className="flex w-80 h-auto justify-start mb-4">
+    <div className="flex w-2/3 h-auto justify-start mb-4 bg-slate-50 p-4 rounded-2xl">
       <div className="mr-4">
         <img
           src={image}
@@ -13,10 +13,10 @@ const ProjectCard = ({ title, description, image, techstack, source }) => {
         ></img>
       </div>
       <div>
-        <p>{title}</p>
-        <p>{description}</p>
-        <p>{techstack}</p>
-        <p>{source}</p>
+        <p className="font-medium text-lg">{title}</p>
+        <p className="text-sm">{techstack}</p>
+        <p className="text-sm text-slate-600">{description}</p>
+        <button><a href={source} target="_blank">Source Code</a></button>
       </div>
     </div>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,29 +5,47 @@ const Projects = () => {
   return (
     <div className="mt-4 pt-16">
       <div className="text-center my-3">
-        <p className="pl-12 text-2xl font-medium">
-          The projects
-        </p>
+        <p className="pl-12 text-2xl font-medium ">The most recent projects</p>
       </div>
       <div className="flex flex-row justify-center items-center gap-5 py-4 ">
         <div className=" w-1/2 place-items-end">
           <ProjectCard
             title={"Freshly LK - Farming site"}
             image={"/img/github-logo.svg"}
+            description={
+              "A webapp for new entered farmers into the market, this system bypasses the middlemen."
+            }
+            techstack={"MERN stack | JWT authentication"}
+            source={"google.com"}
           />
           <ProjectCard
-            title={"Spare parts selling"}
+            title={"Spare parts selling web"}
             image={"/img/github-logo.svg"}
+            description={
+              "A sparepart selling system, sellers can list their products and buyers can buy them."
+            }
+            techstack={"Java | JSP | MySQL | Tailwind CSS"}
+            source={"google.com"}
           />
         </div>
         <div className="w-1/2 pr-8">
           <ProjectCard
-            title={"Expenses tracker - Android app"}
-            image={"/img/github-logo.svg"}
-          />
-          <ProjectCard
             title={"Pavan Uthsara - Youtube channel"}
             image={"/img/youtube-logo.svg"}
+            description={
+              "I support my colleuges by creating helpful videos. Most of them are related to DSA, Java, Sql. [5000+ subscribers]"
+            }
+            techstack={"Tech | Programming | Content Creation"}
+            source={"google.com"}
+          />
+          <ProjectCard
+            title={"Expenses tracker - Android app"}
+            image={"/img/github-logo.svg"}
+            description={
+              "An android app for manage daily expenses and income. Have backup option for user data."
+            }
+            techstack={"Kotlin | xml | Android Studio | Shared Preferences"}
+            source={"google.com"}
           />
         </div>
       </div>
