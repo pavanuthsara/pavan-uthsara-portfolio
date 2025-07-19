61851a10522f6b9ca2e8b13133621e9338b2dd06
Pavan Uthsara <uthsarapavan@gmail.com>
2025-07-19 09:31:19 +0530

    contact section v1.3.1

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -15,7 +15,7 @@ const ProjectCard = ({ title, description, image, techstack, source, source_name
         <p className="font-medium text-lg">{title}</p>
         <p className="text-sm">{techstack}</p>
         <p className="text-sm text-slate-600">{description}</p>
-        <a href={source} target="_blank" ><button className="btn bg-base-100 mt-2">{source_name}</button></a>
+        <a href={source} target="_blank" ><button className="btn bg-base-100 mt-2 ease-out duration-300">{source_name}</button></a>
       </div>
     </div>
   );
